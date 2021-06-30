e1424efb35907999a6c992d0f07d9dcdb9ee9648
n-fukuju <n.fukuju@gmail.com>
2021-06-30 23:12:13 +0900

    メニューレイアウト作成

---
+++ pages/page1.tsx
@@ -0,0 +1,9 @@
+import LayoutComponent from '../components/LayoutComponent';
+
+export default function Page1(){
+    return (
+    <LayoutComponent title="page1" page="page1" content={<>
+        <p>page1 content</p>
+    </>}/>
+    )
+}
