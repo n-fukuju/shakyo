cda78da9c9ca1f391abe64a6bfbf74729de5d244
n-fukuju <n.fukuju@gmail.com>
2021-07-01 16:21:27 +0900

    スクリプト参照時にbasepath追加

--- pages/page1.tsx
+++ pages/page1.tsx
@@ -1,14 +1,16 @@
 import Script from 'next/script';
+import { useRouter } from 'next/router';
 import LayoutComponent from '../components/LayoutComponent';
 
 export default function Page1(){
+    const router = useRouter();
     return (
     <LayoutComponent title="page1" page="page1" content={<>
         <p>page1 content</p>
         <div id="outer-editor" style={{height:"70vh", flex:"1 1 auto", position:"relative", border:"2px solid #bbb"}}>
             <div id="editor" style={{position:"absolute" ,top:0,right:0,bottom:0,left:0}}></div>
         </div>
-        <Script src="/scripts/ace/ace.js" onLoad={()=>{
+        <Script src={router.basePath + "/scripts/ace/ace.js"} onLoad={()=>{
             var ace:any;
             if(ace){
                 var editor = ace.edit("editor");
