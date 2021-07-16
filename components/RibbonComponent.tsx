7e95fb3114b5734e546508c1fb4482ae9abcdb00
n-fukuju <n.fukuju@gmail.com>
2021-07-16 22:51:24 +0900

    GitHub Ribbon （非表示）

---
+++ components/RibbonComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const RibbonComponent: React.FC = ()=>{
+    return (
+      <a href="https://github.com/n-fukuju/shakyo"><img style={{position: "absolute", zIndex: 10000, top: 0, right: 0, border: 0}} src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"/></a>
+    );
+}
+export default RibbonComponent;
